60a0eb63b92705be01c0e17978be6075b27a2055
Lauren Hanna <lhanna7@ford.com>
2022-08-02 09:32:53 -0700

    remove unneeded code

--- src/App.js
+++ src/App.js
@@ -3,15 +3,12 @@ import Nav from './Nav/Nav';
 import Name from './Namesake/Namesake';
 import About from './Pages/About';
 import Work from './Pages/Work';
-import QuoteFetch from './Quotes/quotes';
-
 
 export default function App() {
   return (
     <div className="App">
       <Nav />
       <Name />
-      {/* <QuoteFetch /> */}
       <About />
       <Work />
     </div>
